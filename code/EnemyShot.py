03aed17be1cb5ed6512836e59bc6bdab6df48e13
EvelynPJ <evestudier10@gmail.com>
2025-09-15 15:30:11 -0300

    Hit box Collisions Hud Score

---
+++ code/EnemyShot.py
@@ -0,0 +1,11 @@
+
+from code.Const import ENTITY_SPEED
+from code.Entity import Entity
+
+
+class EnemyShot(Entity):
+    def __init__(self, name, position: tuple):
+        super().__init__(name, position)
+
+    def move(self, ):
+        self.rect.x -= ENTITY_SPEED[self.name]
---
+++ code/PlayerShot.py
@@ -0,0 +1,12 @@
+
+from code.Const import ENTITY_SPEED
+from code.Entity import Entity
+
+
+class PlayerShot(Entity):
+    def __init__(self, name, position: tuple):
+        super().__init__(name, position)
+
+    def move(self, ):
+        self.rect.x += ENTITY_SPEED[self.name]
+
